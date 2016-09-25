6893e46e5d7a960e50c6a1b62ececca8f780303d
curiosity26 <curiosity26@gmail.com>
2016-09-24 22:54:12 -0400

    Add helper add ons

--- README.md
+++ README.md
@@ -72,6 +72,9 @@ Wire-Up JavaScript
 
         <!-- Wire-up the Symfony Form Angular Controller -->
         <script src="{{ asset('bundles/curiosity26angularmaterial/js/mdform.js') }}"></script>
+        
+        <!-- Wire-up the Symfony Angular Addons -->
+        <script src="{{ asset('bundles/curiosity26angularmaterial/js/mdform.js') }}"></script>
 
         // ... The rest of your scripts here
     {% endblock %}
@@ -127,3 +130,107 @@ You can connect you Angular app in the base template or any extended templates. 
         </body>
     </html>
 ```
+
+Material Icons
+--------------
+
+You can use Assetic to load Material Icons from the CDN.
+
+```yaml
+assetic:
+    ...
+    assets:
+        material_icons:
+            inputs:
+                - https://fonts.googleapis.com/icon?family=Material+Icons
+````
+
+```twig
+    {% stylesheets output="style.min.css" "@material_icons" %}
+        <link rel="stylesheet" href="{{ asset_url }}">
+    {% endstylesheets %}
+````
+
+Bundled Addons
+--------------
+
+As I used Angular Material inside Symfony, I decided to make some helper directives to help with things like Toast Alerts and Dialogs.
+
+## sf-alert
+
+The `sf-alert` directive is an **element directive** to display toast alerts. Alerts are automatically displayed when the page load completes.
+
+```HTML
+    <sf-alert sf-alert-action="Yea" sf-alert-parent="#wrapper">This is the message in the alert</sf-alert>
+```
+
+### Attributes
+
+| Attribute | Definition |
+--
+| sf-alert-action | The text as it appears on the action button |
+| sf-alert-position | The position of the alert using any combination of 'top, right, bottom, left, end, start' |
+| sf-alert-auto-wrap | Automatically wrap the contents of the Toast message. *Defaults to true* |
+| sf-alert-capsule | Adds `md-capsule` to the Toast |
+| sf-alert-hide-delay | The number of milliseconds the message displays before hiding the message. 0 disables auto-hiding and is the default value. |
+| sf-alert-parent | The CSS selector of the element the Toast message will align to. Best to use an ID here, as the first item found will be used. |
+
+## sf-dialog-link
+
+The `sf-dialog` directive can be used as an **attribute** or a **class**. I've found this directive to be very useful. 
+
+When the directive is applied to an existing `a` or `md-button` element, the URL in the `href` or `ng-href` attribute are loaded
+and rendered using the `$mdDialog` service.
+
+The `$http` service is used to make the request call and load the response. The `sf-dialog` directive overrides the 
+`$http` service to pass the request header `X-Requested-With: XMLHttpRequest` in order to allow smarty to use 
+`{% app.request.isXmlHttpRequest() %}`.
+
+```twig
+    {# homepage.html.twig #}
+    <md-button ng-href="{{ path('add_object') }}" sf-dialog-link>Open Dialog</md-button>
+```
+
+```twig
+    {# add_object.html.twig #}
+    {% extends app.request.isXmlHttpRequest() ? '::dialog.html.twig' : '::base.html.twig' %}
+    
+    {% block title %}
+        Page Title
+    {% endblock %}
+    
+    {% block body %}
+        ...
+    {% endblock %}
+```
+
+```twig
+    {# dialog.html.twig #}
+    
+    <md-toolbar class="md-primary">
+        <div class="md-toolbar-tools">
+            <h2>{% block title %}{% endblock %}</h2>
+            <span flex></span>
+            <md-button ng-click="dialog.closeDialog()" class="md-icon-button" aria-label="Close">
+                <md-icon>close</md-icon>
+            </md-button>
+        </div>
+    </md-toolbar>
+    {% block body %}{% endblock %}
+```
+
+### Attributes
+
+| Attribute | Definition |
+--
+| href, ng-href | The URL to load in the dialog |
+| sf-dialog-method | The HTTP Method used for the request |
+| sf-dialog-data | Sets the `data` attribute for the `$http` service |
+| sf-dialog-controller | Set the controller to be used for the dialog. A default controller is used which provides a method `closeDialog` that should be bound to a close button. The controller, default or custom, is aliased as 'dialog'. |
+| sf-dialog-success | A callback triggered when `$mdDialog.hide()` is called. |
+| sf-dialog-cancel | A callback triggered when `$mdDialog.cancel()` is called. |
+| sf-dialog-error | A callback triggered when an error is reported the HTTP response with the returned `$err` object as the only parameter. Default behavior shows a Toast with the error message. |
+| sf-dialog-fullscreen |  An option to toggle whether the dialog should show in fullscreen or not. *Defaults to false.* |
+| sf-dialog-has-backdrop | Whether there should be an opaque backdrop behind the dialog. *Default true.* |
+| sf-dialog-click-outside-to-cancel | Whether the user can click outside the dialog to close it. *Default false.* |
+| sf-dialog-disable-parent-scroll | Whether to disable scrolling while the dialog is open. *Default true.* |
---
+++ Resources/public/js/mdaddons.js
@@ -0,0 +1,103 @@
+/**
+ * Created by alexboyce on 9/24/16.
+ */
+
+angular.module('symfony.mdAddons', ['ngMaterial'])
+    .directive('sfAlert', ['$mdToast', '$document', function($mdToast, $document) {
+        return {
+            restrict: 'E',
+            link: function(scope, element, attr) {
+                element.addClass('ng-hide');
+
+                var toast = $mdToast.simple();
+                toast.action(attr['sfAlertAction'] || 'OK');
+                toast.position(attr['sftAlertPosition'] || 'top right');
+                toast.capsule(attr['sfAlertCapsule']);
+                toast.hideDelay(attr['sfAlertHideDelay'] || 0);
+                toast.autoWrap(attr['sfAlertAutoWrap'] == undefined ? true : attr['sfAlertAutoWrap']);
+                toast.parent(!!attr['sfAlertParent'] ? $document[0].querySelector(attr['sfAlertParent'])[0] : $document[0].body);
+                toast.textContent(element.text());
+                // Doing the following safely injects the HTML without opening up to XSS vulnerabilities
+                toast._options.template ='<md-toast md-theme="{{ toast.theme }}" ng-class="{\'md-capsule\': toast.capsule}">' +
+                    '  <div class="md-toast-content">' +
+                    '    <span flex role="alert" aria-relevant="all" aria-atomic="true">' +
+                    '      ' + element.html() +
+                    '    </span>' +
+                    '    <md-button class="md-action" ng-if="toast.action" ng-click="toast.resolve()" ng-class="{\'md-highlight\': toast.highlightAction}">' +
+                    '      {{ toast.action }}' +
+                    '    </md-button>' +
+                    '  </div>' +
+                    '</md-toast>';
+
+                $mdToast.show(toast);
+            }
+        };
+    }])
+    .directive('sfDialogLink', [function() {
+        return {
+            restrict: 'AC',
+            controller: ['$scope', '$mdDialog', '$mdToast', '$element', '$http',
+                function($scope, $mdDialog, $mdToast, $element, $http) {
+                    var href = $element.attr('href') || $element.attr('ng-href');
+                    var method = $element.attr('sfDialogMethod') || 'GET';
+                    var data = $element.attr('sfDialogData') || false;
+                    var controller = $element.attr('sfDialogController') || function($scope, $mdDialog) {
+                            this.closeDialog = function() {
+                                $mdDialog.cancel();
+                            }
+                        };
+                    var fullscreen = $element.attr('sfDialogFullscreen') || false;
+                    var hasBackdrop = $element.attr('sfDialogHasBackdrop') != false;
+                    var escToClose = $element.attr('sfDialogEscapeToClose') != false;
+                    var clickOutsideToClose = $element.attr('sfDialogClickOutsideToClose') || false;
+                    var disableParentScroll = $element.attr('sfDisableParentScroll') != false;
+                    var success = $element.attr('sfDialogSuccess') || false;
+                    var cancel = $element.attr('sfDialogCancel') || false;
+                    var error = $element.attr('sfDialogError') || false;
+                    var options, dialog;
+
+                    $element.on('click', showDialog);
+
+                    function showDialog($event) {
+                        if (!!href) {
+                            $event.preventDefault();
+                            $http({
+                                method: method,
+                                url: href,
+                                data: data,
+                                headers: {
+                                    'X-Requested-With': 'XMLHttpRequest'
+                                }
+                            }).then(function (response) {
+                                options = {
+                                    clickOutsideToClose: clickOutsideToClose,
+                                    template: '<md-dialog flex-xs="90" flex-gt-xs="70" flex-gt-md="50">' +
+                                    '  <md-dialog-content>' +
+                                    '     ' + response.data +
+                                    '  </md-dialog-content>' +
+                                    '</md-dialog>',
+                                    scope: $scope,
+                                    event: $event,
+                                    controller: controller,
+                                    controllerAs: 'dialog',
+                                    preserveScope: true,
+                                    fullscreen: fullscreen,
+                                    hasBackdrop: hasBackdrop,
+                                    escapeToClose: escToClose,
+                                    disableParentScroll: disableParentScroll
+                                };
+
+                                $mdDialog.show(options).then(success, cancel);
+                            }, function ($err) {
+                                if (!!error) {
+                                    error($err);
+                                }
+                                else {
+                                    $mdToast.show($mdToast.simple().textContent($err.getMessage()));
+                                }
+                            });
+                        }
+                    }
+                }]
+        };
+    }]);
